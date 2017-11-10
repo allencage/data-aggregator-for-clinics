a65af33714305d881a7ca914dd721188d485122a
aciuca <alingabriel.ciuca@gmail.com>
2017-11-10 08:17:25 +0200

    Rc 1.0

--- DataAggregatorClincsSG/Program.cs
+++ DataAggregatorClincsSG/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAggregatorClincsSG
 {
@@ -10,6 +9,28 @@ namespace DataAggregatorClincsSG
     {
         static void Main(string[] args)
         {
+            var filesInDirectory = Directory.GetFiles(@"C:\Users\aciuca\Desktop\clinics");
+            var str = new StringBuilder();
+            str.AppendLine("Category, Name, Email");
+            var count = 0;
+            foreach (var file in filesInDirectory)
+            {
+                using (var r = new StreamReader(file))
+                {
+                    string json = r.ReadToEnd();
+                    dynamic array = JsonConvert.DeserializeObject(json);
+                    foreach (var item in array)
+                    {
+                        str.AppendLine(String.Format("{0},{1},{2}",
+                            item.DisplayName, item.Name, item.EMailAppointmentReservationUrl));
+                        count++;
+                        Console.WriteLine(count);
+                    }
+                }
+            }
+            File.WriteAllText(@"C:\Users\aciuca\Desktop\clinics.csv", str.ToString());
+            Console.Write("Total number of records: {0}", count);
+            Console.Read();
         }
     }
 }
